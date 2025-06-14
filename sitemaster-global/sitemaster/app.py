775c8738d9976adab16c70d336db9ea0a4946e25
Player232006 <sakshamsinghmayoor@gmail.com>
2025-06-14 23:27:57 +0530

    Update app.py

--- sitemaster-global/sitemaster/app.py
+++ sitemaster-global/sitemaster/app.py
@@ -1,1 +1,14 @@
-<GLOBAL_APP_CODE_WITH_CONFIG_AND_LANG_SUPPORT>
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=10000)
+ 
